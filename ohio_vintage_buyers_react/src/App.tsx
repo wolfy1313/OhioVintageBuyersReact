319ee205984e1390dd84479676b1f1d8fc687c6b
jeremyHarrell <hillvulf@gmail.com>
2023-05-23 11:00:18 -0500

    style form elements from form spree. include tailwind css

--- ohio_vintage_buyers_react/src/App.tsx
+++ ohio_vintage_buyers_react/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className='font-black text-5xl'>Ohio is for Vintage Lovers</h1>
       </header>
+      <body className='bg-yellow-300 min-h-screen min-w-full'>
+        <ContactForm />
+      </body>
     </div>
   );
 }
