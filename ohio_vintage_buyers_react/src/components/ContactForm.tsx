3f5b8d464880c8effeb575e14ae2b1fa34ffc296
jeremyHarrell <hillvulf@gmail.com>
2023-05-24 08:39:29 -0500

    complete intitial form styling

--- ohio_vintage_buyers_react/src/components/ContactForm.tsx
+++ ohio_vintage_buyers_react/src/components/ContactForm.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const ContactForm = () => {
   return (
     <div>
-      <div>
-        <form className='w-full max-w-sm' method="POST" action="https://formspree.io/f/mjvddkno" encType="multipart/form-data">
+      <div className='flex justify-center'>
+        <form className='w-full max-w-sm mt-10 border-4 px-3 py-3 border-gray-600' method="POST" action="https://formspree.io/f/mjvddkno" encType="multipart/form-data">
           <div className='md:flex md:items-center mb-6'>
-            <div className='md-w-1/3'>
+            <div className='md:w-1/3'>
               <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'>Your Name:
               </label>
             </div>
@@ -23,17 +23,30 @@ const ContactForm = () => {
               <input className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id="inline-email" type="text" placeholder="Your Email" />
             </div>
           </div>
-          <label>Details about your items:
-            <textarea name="message" placeholder="Details of your items"></textarea>
-          </label>
-          <label>Include any pertinent images here
-            <input type="file" name="attachment" accept="image/png, image/jpeg" />
-          </label>
+          <div className='md:flex md:items-center mb-6'>
+            <div className='md:w-1/3'>
+              <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'>Details about your items:
+              </label>
+            </div>
+            <div className='md:w-2/3'>
+              <textarea name="message" className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500' id="inline-email" placeholder="Details of your items"></textarea>
+            </div>
+          </div>
+          <div className='md:flex md:items-center mb-6'>
+            <div className='md:w-1/3'>
+              <label className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'>Include any pertinent images here
+              </label>
+            </div>
+            <div className='md:w-2/3'>
+              <input type="file" name="attachment" accept="image/png, image/jpeg" className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" />
+              <p className="mt-1 text-sm text-gray-500 font-bold dark:text-gray-500" id="file_input_help">PNG or JPG images only please.</p>
+            </div>
+          </div>
           <div className="md:flex md:items-center">
             <div className="md:w-1/3"></div>
             <div className="md:w-2/3">
-              <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                Sign Up
+              <button className="shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                Get Vintage'd
               </button>
             </div>
           </div>
